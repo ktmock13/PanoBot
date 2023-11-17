425f788496ce62474772b444fa9cc47644786248
Keith Mock <kmock@apple.com>
2023-11-17 14:51:16 -0800

    update

--- display.py
+++ display.py
@@ -17,16 +17,16 @@ class Display:
 
     # Initialize an Image for logging purposes
     self.logImage = Image.new("1", (self.display.width, self.display.height))
-    # Initialize log state
+    self.drawLog = ImageDraw.Draw(self.logImage)
     self.log = ["11111111111111111111","22122222222222222222", "33333333333333333333", "44444444444444444444", "55555555555555555555555"]
 
     # Initialize menu image
     self.menuImage = Image.new("1", (self.display.width, self.display.height))
+    self.drawMenu = ImageDraw.Draw(self.menuImage)
     self.menu = ["a","b", "c", "d", "e"]
 
 
     # Get drawing object to draw on image.
-    self.draw = ImageDraw.Draw(self.logImage if isLogging else self.menuImage)
 
     # Draw a black filled box to clear the image.
     self.draw.rectangle((0, 0, self.display.width, self.display.height), outline=0, fill=0)
@@ -38,10 +38,10 @@ class Display:
     # Draw a black filled box to clear the image.
     self.draw.rectangle((0, 0, self.display.width, self.display.height), outline=0, fill=0)
     # Write four lines of text.
-    self.draw.text((0,-2), self.log[3], font=self.font, fill=255)
-    self.draw.text((0,6), self.log[2], font=self.font, fill=255)
-    self.draw.text((0,14), self.log[1], font=self.font, fill=255)
-    self.draw.text((0,22), self.log[0], font=self.font, fill=255)
+    self.drawLog.text((0,-2), self.log[3], font=self.font, fill=255)
+    self.drawLog.text((0,6), self.log[2], font=self.font, fill=255)
+    self.drawLog.text((0,14), self.log[1], font=self.font, fill=255)
+    self.drawLog.text((0,22), self.log[0], font=self.font, fill=255)
 
     # Display image.
     self.display.image(self.logImage)
@@ -51,10 +51,10 @@ class Display:
     # Draw a black filled box to clear the image.
     self.draw.rectangle((0, 0, self.display.width, self.display.height), outline=0, fill=0)
     # Write four lines of text.
-    self.draw.text((0,-2), self.menu[3], font=self.font, fill=255)
-    self.draw.text((0,6), self.menu[2], font=self.font, fill=255)
-    self.draw.text((0,14), self.menu[1], font=self.font, fill=255)
-    self.draw.text((0,22), self.menu[0], font=self.font, fill=255)
+    self.drawMenu.text((0,-2), self.menu[3], font=self.font, fill=255)
+    self.drawMenu.text((0,6), self.menu[2], font=self.font, fill=255)
+    self.drawMenu.text((0,14), self.menu[1], font=self.font, fill=255)
+    self.drawMenu.text((0,22), self.menu[0], font=self.font, fill=255)
 
     # Display image.
     self.display.image(self.menuImage)
