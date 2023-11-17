c5acc0e411cb787fc8041e38bca0599cb4c79086
Keith Mock <kmock@apple.com>
2023-11-17 14:46:25 -0800

    update

--- display.py
+++ display.py
@@ -51,10 +51,10 @@ class Display:
     # Draw a black filled box to clear the image.
     self.draw.rectangle((0, 0, self.display.width, self.display.height), outline=0, fill=0)
     # Write four lines of text.
-    self.draw.text((0,0), self.menu[3], font=self.font, fill=255)
-    self.draw.text((0,8), self.menu[2], font=self.font, fill=255)
-    self.draw.text((0,16), self.menu[1], font=self.font, fill=255)
-    self.draw.text((0,25), self.menu[0], font=self.font, fill=255)
+    self.draw.text((0,-2), self.menu[3], font=self.font, fill=255)
+    self.draw.text((0,6), self.menu[2], font=self.font, fill=255)
+    self.draw.text((0,14), self.menu[1], font=self.font, fill=255)
+    self.draw.text((0,22), self.menu[0], font=self.font, fill=255)
 
     # Display image.
     self.display.image(self.logImage)
