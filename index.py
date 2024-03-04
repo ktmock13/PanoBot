85c03591b4bd6d93a24aa8125aa7fd40a7050644
Keith Mock <kmock@apple.com>
2024-03-03 21:13:02 -0800

    smooth operation

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from scene import Scene
 from display import Display
+import time
 def main():
   # Settings
   CAMERA_NAME = "iPhone 15 Pro Max";
@@ -9,14 +10,17 @@ def main():
   PANO_FOV_Y = 60 #degrees
   OVERLAP_PERCENT = .15 # whole number ex: 15 = 15 percent
 
-  # Shot interval, time spent between snapshots
-  SHOT_INTERVAL = 250 #milliseconds
-
   # Initialize and run
-  scene = Scene(CAMERA_FOV, CAMERA_ASPECT, CAMERA_NAME, PANO_FOV_X, PANO_FOV_Y, OVERLAP_PERCENT)
-  scene.runScene(SHOT_INTERVAL);
+  display = Display()
 
-
+  scene = Scene(CAMERA_FOV, CAMERA_ASPECT, CAMERA_NAME, PANO_FOV_X, PANO_FOV_Y, OVERLAP_PERCENT, display)
+  scene.printInfo()
+  time.sleep(3)
+  scene.camera.printInfo();
+  time.sleep(3)
+  scene.runScene(250)
+  display.clearLog()
 if __name__ == "__main__":
     main()
+    
     # main()
