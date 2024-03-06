146a7a7b084e2c860bb11b9b85b98bdc9fffcf3a
Keith Mock <kmock@apple.com>
2024-03-05 18:50:12 -0800

    update

--- constants.py
+++ constants.py
@@ -6,4 +6,4 @@ PANO_FOV_X = 120 #degrees
 PANO_FOV_Y = 60 #degrees
 OVERLAP_PERCENT = .15 # whole number ex: 15 = 15 percent
 DELAY_BEFORE_SHOT=250 #ms - to to account for shake on tripod
-DELAY_AFTER_SHOT=250 #ms - to account for image exposure
+DELAY_AFTER_SHOT=4000 #ms - to account for image exposure
