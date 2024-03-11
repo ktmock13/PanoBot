103b46b3c9f0f3b507d467d1cccec90244cfd4e8
Keith Mock <kmock@apple.com>
2024-03-10 19:20:43 -0700

    update

--- menu.py
+++ menu.py
@@ -15,20 +15,19 @@ display.rotation = 0  # Adjust rotation as needed
 display.fill(0)
 display.show()
 
-# Setup inpuot buttons
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Up button
-GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Select button
-GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Down button
 
 # Menu items definition
 menu_items = [
-    {"id": 1, "value": 8.75, "increment": 0.5},
-    {"id": 2, "value": 15.4, "increment": 1.0},
-    {"id": 3, "value": 500, "increment": 5.0},
-    {"id": 4, "value": 250, "increment": 10.0},
-    {"id": 5, "value": 128, "increment": 2.0},
-    {"id": 6, "value": 68, "increment": 0.1},
-    {"id": 7, "value": "START"}
+    {"id": "cameraFOV", "value": 15.5, "increment": 0.5},
+    {"id": "cameraAspectRatio", "value": 0.75, "increment": 0.5},
+    {"id": "focusDelay", "value": 500, "increment": 100},
+    {"id": "exposureDelay", "value": 500, "increment": 100},
+    {"id": "rangeX", "value": 120, "increment": 5},
+    {"id": "rangeY", "value": 60, "increment": 5},
+    {"id": "overlapPercent", "value": 0.3, "increment": 0.05},
+    {"id": "robotSpeed", "value": 7, "increment": 1},
+    # Add more menu items as needed
+    {"id": "action-start", "value": "START"}  # 'START' as the last menu item
 ]
 
 # Initial state
@@ -36,66 +35,97 @@ selected_index = 0
 editing_mode = False
 
 # Create an image with PIL
-screen_width, screen_height = 128, 160  # Screen dimensions
-image = Image.new("RGB", (screen_width, screen_height), "black")
+image = Image.new("1", (screen_width, screen_height), "black")
+# image = image.rotate(90, expand=True)
 draw = ImageDraw.Draw(image)
+# font_path = "ret.ttf"  # Update this to your font's path
+# font_size = 6
+# font = ImageFont.truetype(font_path, font_size)
 font = ImageFont.load_default()  # Default font, adjust as needed
+print(font.getsize("hello"))
 
 def draw_menu():
     draw.rectangle((0, 0, screen_width, screen_height), fill="black")  # Clear screen
+
     for index, item in enumerate(menu_items):
         text = str(item["value"])
         text_width, text_height = draw.textsize(text, font=font)
-        if item["value"] == "START":
-            x = (screen_width - text_width) / 2  # Center 'START' horizontally
-        else:
-            x = 5  # Regular items aligned to the left
-
-        y = 10 + index * (screen_height // len(menu_items))
+        
+        # Calculate y position based on index, adjust as needed
+        y = 3 + index * (screen_height // len(menu_items))
+        
         if index == selected_index:
-            draw.rectangle((0, y, screen_width, y + text_height + 5), fill="white")
-            draw.text((x, y), text, font=font, fill="black")
+            # Highlight selected item: draw a white rectangle behind the text
+            draw.rectangle((0, y, screen_width, y + text_height), fill="white")
+            draw.text((2, y), text, font=font, fill="black")
         else:
-            draw.text((x, y), text, font=font, fill="white")
+            draw.text((2, y), text, font=font, fill="white")
 
-    image.show()  # Display the image, replace with your display method
+    rotated_image = image.rotate(90, expand=True)
 
-def up_callback(channel):
+    display.image(rotated_image)
+    display.show()
+
+def clear_screen():
+    display.image(Image.new("1", (screen_width, screen_height), "black"))
+    display.show()
+
+def change_selection(direction):
     global selected_index
-    if selected_index > 0:
+    if direction == "up" and selected_index > 0:
         selected_index -= 1
-        draw_menu()
-
-def down_callback(channel):
-    global selected_index
-    if selected_index < len(menu_items) - 1:
+    elif direction == "down" and selected_index < len(menu_items) - 1:
         selected_index += 1
-        draw_menu()
-
-def select_callback(channel):
-    global editing_mode, selected_index
-    if menu_items[selected_index]["value"] == "START":
-        print("Starting...")  # Or perform the start action
-        sceneSettings =  {item['id']: item['value'] for item in menu_items if not item['id'].startswith("action")}
-        scene = Scene(**sceneSettings)
-        scene.runScene() 
-    elif not editing_mode:
-        editing_mode = True
-    else:
-        editing_mode = False
     draw_menu()
 
-# Define the button press event callbacks
-GPIO.add_event_detect(16, GPIO.FALLING, callback=up_callback, bouncetime=300)
-GPIO.add_event_detect(20, GPIO.FALLING, callback=select_callback, bouncetime=300)
-GPIO.add_event_detect(21, GPIO.FALLING, callback=down_callback, bouncetime=300)
+def toggle_editing_mode():
+    global editing_mode
+    editing_mode = not editing_mode
+    draw_menu()
 
-# Initial draw
-draw_menu()
+def adjust_value(direction):
+    if "increment" in menu_items[selected_index]:  # Check if the item is adjustable
+        increment = menu_items[selected_index]["increment"]
+        if direction == "up":
+            menu_items[selected_index]["value"] += increment
+        elif direction == "down":
+            menu_items[selected_index]["value"] -= increment
+        menu_items[selected_index]["value"] = round(menu_items[selected_index]["value"], 2)  # Adjust rounding precision
+    draw_menu()
 
-# Keep the script running to listen for button inputs
-try:
-    while True:
-        time.sleep(0.1)  # Small delay to reduce CPU usage
-except KeyboardInterrupt:
-    GPIO.cleanup()  # Clean up GPIO on CTRL+C exit
+
+def run_menu():
+    GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Up button
+    GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Select button
+    GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Down button
+    draw_menu()
+    def up_callback(channel):
+      change_selection("up")
+
+    def down_callback(channel):
+      change_selection("down")
+
+    def select_callback(channel):
+        global editing_mode
+        if menu_items[selected_index]["value"] == "START":
+            print("Starting...")  # Or perform the start action
+            sceneSettings =  {item['id']: item['value'] for item in menu_items if not item['id'].startswith("action")}
+            scene = Scene(**sceneSettings)
+            scene.runScene() 
+        elif not editing_mode:
+            toggle_editing_mode()
+        else:
+            toggle_editing_mode()
+
+    # Define the button press event callbacks
+    GPIO.add_event_detect(16, GPIO.FALLING, callback=up_callback, bouncetime=300)
+    GPIO.add_event_detect(20, GPIO.FALLING, callback=select_callback, bouncetime=300)
+    GPIO.add_event_detect(21, GPIO.FALLING, callback=down_callback, bouncetime=300)
+
+    try:
+        while True:
+            time.sleep(0.1)  # Small delay to reduce CPU usage
+    except KeyboardInterrupt:
+        GPIO.cleanup()  # Clean up GPIO on CTRL+C exit
+        clear_screen()
+
